7eb7aaa4a1fba39029464f1ff2d121571851d651
Hristiyan Mitov <hristiyan.mitov@limechain.tech>
2025-09-30 11:16:10 +0300

    chore: FIxed format

--- test/HLInterop.t.sol
+++ test/HLInterop.t.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.30;
 import "forge-std/Test.sol";
 import "../src/HLInterop.sol";
 
-
 contract HLInteropTest is Test {
-
-    function test() public {
-    }
+    function test() public {}
 }
