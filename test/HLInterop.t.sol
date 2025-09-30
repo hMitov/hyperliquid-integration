dde6e30307603607baf13c03be9a4c7f3dd4c5d3
Hristiyan Mitov <hristiyan.mitov@limechain.tech>
2025-09-30 11:15:17 +0300

    chore: Fixed failing tests with dummy test

---
+++ test/HLInterop.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.30;
+
+import "forge-std/Test.sol";
+import "../src/HLInterop.sol";
+
+
+contract HLInteropTest is Test {
+
+    function test() public {
+    }
+}
